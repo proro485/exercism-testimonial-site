9df1ef5af7af6c811c8261da11e0c23197a6fc48
Rohit Singh Bisht <rohitsb485@gmail.com>
2022-03-23 11:16:02 +0530

    Fixed some issues with link in explore

--- src/components/NavbarButtons.js
+++ src/components/NavbarButtons.js
@@ -5,7 +5,7 @@ export default function NavbarButtons(props) {
   return (
     <div className="hidden xl:ml-8 xl:flex xl:items-center xl:cursor-pointer xl:pt-1">
       <img className="mr-2" src={props.image} alt="" />
-      <Link to={`${props.description !== "dashboard" ? `/${props.description}` : "/"}`} className={`font-poppins font-semibold ${props.selected ? "text-darkPurple" : "text-lightPurple"}`}>
+      <Link to={`${props.description !== "dashboard" ? `/${props.description}` : ""}`} className={`font-poppins font-semibold ${props.selected ? "text-darkPurple" : "text-lightPurple"}`}>
         {props.title}
       </Link>
     </div >
--- src/components/NavbarButtonsExplore.js
+++ src/components/NavbarButtonsExplore.js
@@ -5,7 +5,7 @@ export default function NavbarButtons(props) {
   return (
     <div className="flex items-center cursor-pointer p-2 justify-center hover:bg-faintPurple">
       <img className="mr-2 h-5" src={props.image} alt="" />
-      <Link to={`${props.description !== "dashboard" ? `/${props.description}` : "/"}`} className={`font-poppins font-semibold ${props.selected ? "text-darkPurple" : "text-lightPurple"}`}>
+      <Link to={`${props.description !== "dashboard" ? `/${props.description}` : ""}`} className={`font-poppins font-semibold ${props.selected ? "text-darkPurple" : "text-lightPurple"}`}>
         {props.title}
       </Link>
     </div >
