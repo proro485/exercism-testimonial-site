465fed72bbf72260ed8eb74b0f759248a94e17c4
Rohit Singh Bisht <rohitsb485@gmail.com>
2022-03-23 09:18:58 +0530

    Made the sort dropdown a button

--- src/components/SortDropdown.js
+++ src/components/SortDropdown.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import dropdownBig from '../assets/dropdownBig.svg'
+import React, { useState } from 'react';
+import dropdownBig from '../assets/dropdownBig.svg';
 
 export default function SortDropdown(props) {
   const [dropDown, setDropDown] = useState(false);
@@ -15,24 +15,23 @@ export default function SortDropdown(props) {
   return (
     <div className={`list_headerLeft cursor-pointer md:w-1/2 lg:w-1/4 xl:w-1/5 relative mx-1 sm:mx-2 text-sm sm:text-base`}
       onClick={() => setDropDown(!dropDown)}
-      onMouseLeave={() => setDropDown(false)}
     >
 
-      <div className={`sort ${dropDown ? "rounded-t-[5px] hover:bg-yetAnotherFaintPurple" : "rounded-[5px]"} flex bg-faintPurple justify-between`}
+      <button className={`hover:bg-purplePaginationBgActive w-full flex items-center ${dropDown ? "rounded-t-[5px] hover:bg-yetAnotherFaintPurple" : "rounded-[5px]"} flex bg-faintPurple justify-between`}
         onClick={() => props.setSelected(props.selected)}
       >
-        <button className="px-5 py-3 text-lightPurple font-poppins">{sortType[props.selected]}</button>
+        <div className="px-5 py-3 text-lightPurple font-poppins">{sortType[props.selected]}</div>
         {
           !dropDown && <img className="pr-5" src={dropdownBig} alt="" />
         }
-      </div>
+      </button>
       {
-        dropDown && <div className={`sort rounded-b-[5px] flex bg-faintPurple absolute z-10 justify-between w-full ${dropDown && "hover:bg-yetAnotherFaintPurple"}`}
+        dropDown && <button className={`sort rounded-b-[5px] flex bg-faintPurple absolute z-10 justify-between w-full ${dropDown && "hover:bg-yetAnotherFaintPurple"}`}
           onClick={() => props.setSelected((props.selected + 1) % 2)}>
 
-          <button className="px-5 py-3 text-lightPurple font-poppins">{sortType[(props.selected + 1) % 2]}</button>
+          <div className="px-5 py-3 text-lightPurple font-poppins">{sortType[(props.selected + 1) % 2]}</div>
 
-        </div>
+        </button>
       }
     </div>
   );
