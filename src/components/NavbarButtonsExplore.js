414d0391b6ac0701f004746146d9dc0616885bce
Rohit Singh Bisht <rohitsb485@gmail.com>
2022-03-23 11:18:54 +0530

    Fixed the issue with clicking on links in navbar

--- src/components/NavbarButtonsExplore.js
+++ src/components/NavbarButtonsExplore.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default function NavbarButtons(props) {
   return (
-    <div className="flex items-center cursor-pointer p-2 justify-center hover:bg-faintPurple">
+    <Link to={`${props.description !== "dashboard" ? `/${props.description}` : "/"}`} className="flex items-center cursor-pointer p-2 justify-center hover:bg-faintPurple">
       <img className="mr-2 h-5" src={props.image} alt="" />
-      <Link to={`${props.description !== "dashboard" ? `/${props.description}` : ""}`} className={`font-poppins font-semibold ${props.selected ? "text-darkPurple" : "text-lightPurple"}`}>
+      <div className={`font-poppins font-semibold ${props.selected ? "text-darkPurple" : "text-lightPurple"}`}>
         {props.title}
-      </Link>
-    </div >
+      </div>
+    </Link>
   );
 }
